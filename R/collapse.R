4bc6cb9db64d611c96ce0e299894ae715d1dddc1
mataddy <mataddy@outlook.com>
2013-10-25 09:12:07 -0500

    don't expand v in collapse

--- R/collapse.R
+++ R/collapse.R
@@ -3,9 +3,12 @@
 
 collapse <- function(v,x,mu=NULL,bins=NULL,listx=TRUE){
 
-	if(inherits(v,c("Matrix","simple_triplet_matrix")))
-		v <- as.matrix(v)
-	v <- as.data.frame(v)
+	if(inherits(v,c("simple_triplet_matrix"))){
+    v <- sparseMatrix(i=v$i,j=v$j,x=v$v,
+            dims=dim(v),dimnames=dimnames(v))
+  }
+	if(is.null(dim(v))) v <- as.matrix(v)
+  rownames(v) <- NULL
 
   if(inherits(x,"data.frame")){
     if(ncol(x)>1) x <- as.matrix(x)
@@ -19,7 +22,7 @@ collapse <- function(v,x,mu=NULL,bins=NULL,listx=TRUE){
   }
   if(inherits(x,"simple_triplet_matrix")){
     x <- sparseMatrix(i=x$i,j=x$j,x=x$v,
-    dims=dim(x),dimnames=dimnames(x))
+      dims=dim(x),dimnames=dimnames(x))
   }
   x=as(x,"dgCMatrix") 
   if(is.null(colnames(x))) colnames(x) <- 1:ncol(x)
@@ -45,7 +48,7 @@ collapse <- function(v,x,mu=NULL,bins=NULL,listx=TRUE){
             breaks=unique(quantile(vj, qs)), 
             include.lowest=TRUE))
   }
-  B <- apply(v,2,cutit)
+  B <- apply(v,2,cutit) 
   I <- interaction(as.data.frame(B), drop=TRUE)
   v <- apply(v,2,function(vj) tapply(as.numeric(vj), I, mean))
   nbin <- table(I)
