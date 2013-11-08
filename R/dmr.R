704e98aa3083aeae1018394dac433461715ad6ab
Matt Taddy <taddy@chicagobooth.edu>
2013-11-08 17:03:08 -0600

    add GC

--- R/dmr.R
+++ R/dmr.R
@@ -9,6 +9,8 @@ setClass("dmrcoef",
 onerun <- function(xj, argl){
   argl$y <- xj
   fit <- do.call(gamlr,argl)
+  gc()
+  print(ls())
   ## below only works if you've specified an outfile in makeCluster
   if(length(fit$lambda)<argl$nlambda) print(colnames(xj))
   return(fit)
