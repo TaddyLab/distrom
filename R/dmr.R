513877f48163ff31862a4bae5e59867cabc14514
mataddy <mataddy@outlook.com>
2015-03-16 15:37:50 -0500

    final fix for mle existence check

--- R/dmr.R
+++ R/dmr.R
@@ -6,14 +6,14 @@ setClass("dmrcoef", contains="dgCMatrix")
 ## inner loop function
 onerun <- function(xj, argl){
   argl$y <- xj
-  #if(length(argl$free)>0){ #check existence of mle portion
-  #  xfnz <- as.matrix(argl$x[drop(argl$y>0),argl$free])
-  #  Q <- qr(cbind(1,xfnz))
-  #  fullrank <- Q$pivot[2:Q$rank]-1
-  #  #argl$free <- argl$free[fullrank]
-  #  argl$ridge <- rep(0,ncol(argl$x))
-  #  argl$ridge[argl$free[-fullrank]] <- 1e-5
-  #}
+  if(length(argl$free)>0){ #check existence of mle portion
+   xfnz <- as.matrix(argl$x[drop(argl$y>0),argl$free])
+   Q <- qr(cbind(1,xfnz))
+   fullrank <- Q$pivot[2:Q$rank]-1
+   if(is.null(argl$varweight)) argl$varweight <- rep(1,ncol(argl$x))
+   argl$varweight[argl$free[-fullrank]] <- Inf
+   argl$free <- argl$free[fullrank]
+  }
   if(argl$cv) fit <- do.call(cv.gamlr,argl)
   else fit <- do.call(gamlr,argl)
   return(fit)
